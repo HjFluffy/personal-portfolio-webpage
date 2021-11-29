07e376850042ac7dd553089142168292075411b8
HjFluffy <91278027+HjFluffy@users.noreply.github.com>
2021-11-28 18:42:30 -0700

    Add files via upload

---
+++ codejournal.js
@@ -0,0 +1,18 @@
+let string1 = "hello world" // this is a string literal example
+
+let string2 = new string("Hello world!") //using a 'constructor'
+
+let mynum = 20123421;
+
+let mydecimal = 73.4 // could also be called float
+
+let myArray = [] //this is an empty array
+
+// object
+
+const myCar = {
+    make: "toyota",
+    color: "blue",
+    vin: "im not giving you my vin number",
+    year: "1996"
+}
